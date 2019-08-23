81bbe2ff61d9a0ad6d4b8448247be86f5d8d9ce3
Mohammad B.Yaseen <mohammad.yaseen1988@gmail.com>
2019-08-23 14:16:02 +0300

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./App.css"
+
+function Header() {
+    return (
+        <header className="navBar">This is header</header>
+    )
+}
+export default Header
---
+++ src/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+function MainContent() {
+    return(
+        <main>
+            <p>This React App, parent/child components .....!</p>
+        </main>
+    )
+}
+export default MainContent
